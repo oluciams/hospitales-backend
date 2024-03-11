8232015a7d85fd04c7395e27920987af2780e349
Olga Marin <oluciams84@gmail.com>
2024-03-11 16:26:38 -0500

    add model, controllers and user routes

---
+++ controllers/usuarios.js
@@ -0,0 +1,32 @@
+const { response } = require("express");
+
+const Usuario = require("../models/usuario");
+
+const getUsuarios = async (req, res) => {
+
+  const usuarios = await Usuario.find({}, 'nombre email role google'); 
+
+  res.json({
+    ok: true,
+    usuarios
+  });
+};
+
+const crearUsuario = async (req, res = response) => {
+
+  const { email, password, nombre } = req.body;
+
+  const usuario = new Usuario(req.body);
+
+  await usuario.save();
+
+    res.json({
+      ok: true,
+      usuario,
+  });
+};
+
+module.exports = {
+  getUsuarios,
+  crearUsuario,
+};
